5f266b0d2eab7de88eee43042dfddd55e5a7e578
MHMasoon <mhmasoon2000@gmail.com>
2024-10-16 01:06:46 +0330

    add ability to toggle cells

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use crossterm::event::Event;
 use crossterm::event::DisableMouseCapture;
 use crossterm::event::KeyCode;
 use crossterm::event::KeyModifiers;
+use crossterm::event::MouseEventKind;
+use crossterm::event::MouseButton;
 use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::disable_raw_mode;
 use crossterm::terminal::size;
@@ -14,13 +16,19 @@ use crossterm::terminal::Clear;
 use crossterm::terminal::ClearType;
 use crossterm::terminal::EnterAlternateScreen;
 use crossterm::terminal::LeaveAlternateScreen;
+use crossterm::style::SetBackgroundColor;
 use crossterm::style::Color;
+use crossterm::style::Print;
 use crossterm::queue;
 use crossterm::cursor;
 
 fn main() {
+    // ToDo : overwrite the size() function to always return usize
+    // so you don't have to convert it everywhere you need it
+
     // Constants
     const CELL_COLOR: Color = Color::Yellow;
+    const BACKGROUND_COLOR: Color = Color::Black;
 
     // Ignore first row and column for easier 1-based indexing
     let mut cells: Vec<Vec<bool>> = vec![
@@ -33,7 +41,14 @@ fn main() {
 
     // Configure terminal settings for optimal display and usage
     enable_raw_mode();
-    queue!(stdout, EnterAlternateScreen, EnableMouseCapture, cursor::Hide);
+    queue!(
+        stdout,
+        EnterAlternateScreen,
+        SetBackgroundColor(BACKGROUND_COLOR),
+        Clear(ClearType::All),
+        EnableMouseCapture,
+        cursor::Hide
+    );
     stdout.flush();
 
     // ToDo
@@ -48,9 +63,34 @@ fn main() {
                     _ => {},
                 }
             },
+            Event::Mouse(mouse_event) => {
+                match mouse_event.kind {
+                    MouseEventKind::Down(MouseButton::Left) => {
+                        let column = mouse_event.column;
+                        let row = mouse_event.row;
+                        // ToDo : create a macro to toggle a bools value
+                        // ToDo : do something for this repeatative use of into()
+                        if cells[column as usize][row as usize] {
+                            cells[column as usize][row as usize] = false;
+                            queue!(stdout,SetBackgroundColor(BACKGROUND_COLOR));
+                        } else {
+                            cells[column as usize][row as usize] = true;
+                            queue!(stdout,SetBackgroundColor(CELL_COLOR));
+                        }
+                        queue!(
+                            stdout,
+                            cursor::MoveTo(column, row),
+                            Print(' '),
+                            cursor::MoveTo(0, 0),
+                        );
+                        stdout.flush();
+                    },
+                    _ => {}
+                }
+            }
             _ => {},
         }
-        println!("{:?}", event);
+        // println!("{:?}", event);
         
     }
 
