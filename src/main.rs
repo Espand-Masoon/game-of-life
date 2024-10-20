cd43a8f010a74f4d8a65a3f303ea09d683918b4e
MHMasoon <mhmasoon2000@gmail.com>
2024-10-20 15:12:26 +0330

    Implement cell generation logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::stdout;
 use std::io::Write;
+use std::time::Duration;
+use crossterm::event::poll;
 use crossterm::event::read;
 use crossterm::event::EnableMouseCapture;
 use crossterm::event::KeyEvent;
@@ -29,16 +31,21 @@ fn main() {
     // Constants
     const CELL_COLOR: Color = Color::Yellow;
     const BACKGROUND_COLOR: Color = Color::Black;
+    const TOP_MARGIN: u16 = 2;
+    const BOTTOM_MARGIN: u16 = 1;
 
     // Global variables
+    // ToDo: Create two variables with different types to avoid coversion
     let mut terminal_width = size().unwrap().0;
     let mut terminal_height = size().unwrap().1;
 
     // Create a matrix to represent the terminal sheet
+    // rows of cells
     let mut cells: Vec<Vec<bool>> = vec![
-        vec![false; (terminal_height).into()];
-        (terminal_width).into()
+        vec![false; terminal_width.into()];
+        terminal_height.into()
     ];
+    let mut next_gen_cells = cells.clone();
 
     // ToDo
     let mut stdout = stdout();
@@ -66,55 +73,107 @@ fn main() {
 
     // ToDo: Comment
     loop {
-        let event = read().unwrap();
-        match event {
-            Event::Key(key_event) => {
-                match (key_event.code, key_event.modifiers) {
-                    (KeyCode::Char('c'), KeyModifiers::CONTROL) |
-                    (KeyCode::Char('q'), KeyModifiers::NONE) => {
-                        break;
-                    },
-                    _ => {},
-                }
-            },
-            Event::Mouse(mouse_event) => {
-                match mouse_event.kind {
-                    MouseEventKind::Down(MouseButton::Left) => {
-                        let row = mouse_event.row;
-                        if row == 0 || row == terminal_height - 1 {}
-                        else {
-                            let column = mouse_event.column;
-                            // ToDo : create a macro to toggle a bools value
-                            // ToDo : do something for this repeatative use of into()
-                            if cells[column as usize][row as usize] {
-                                cells[column as usize][row as usize] = false;
-                                queue!(stdout,SetBackgroundColor(BACKGROUND_COLOR));
-                            } else {
-                                cells[column as usize][row as usize] = true;
-                                queue!(stdout,SetBackgroundColor(CELL_COLOR));
+        // Wait for an event
+        if poll(Duration::from_millis(1000)).unwrap() {
+            match read().unwrap() {
+                Event::Key(key_event) => {
+                    match (key_event.code, key_event.modifiers) {
+                        (KeyCode::Char('c'), KeyModifiers::CONTROL) |
+                        (KeyCode::Char('q'), KeyModifiers::NONE) => {
+                            break;
+                        },
+                        _ => {},
+                    }
+                },
+                Event::Mouse(mouse_event) => {
+                    match mouse_event.kind {
+                        MouseEventKind::Down(MouseButton::Left) => {
+                            let row = mouse_event.row;
+                            if row < TOP_MARGIN || row >= terminal_height - BOTTOM_MARGIN {}
+                            else {
+                                let column = mouse_event.column;
+                                // ToDo : create a macro to toggle a bools value
+                                // ToDo : do something for this repeatative use of into()
+                                if cells[row as usize][column as usize] {
+                                    cells[row as usize][column as usize] = false;
+                                    queue!(stdout,SetBackgroundColor(BACKGROUND_COLOR));
+                                } else {
+                                    cells[row as usize][column as usize] = true;
+                                    queue!(stdout,SetBackgroundColor(CELL_COLOR));
+                                }
+                                queue!(
+                                    stdout,
+                                    cursor::MoveTo(column, row),
+                                    Print(' '),
+                                    cursor::MoveTo(0, 0),
+                                );
+                                stdout.flush();
                             }
-                            queue!(
-                                stdout,
-                                cursor::MoveTo(column, row),
-                                Print(' '),
-                                cursor::MoveTo(0, 0),
-                            );
-                            stdout.flush();
+                        },
+                        _ => {}
+                    }
+                }
+                _ => {},
+            }
+        }
+
+        // Generate next generation cells
+        for row_index in (TOP_MARGIN as usize)..(terminal_height - BOTTOM_MARGIN).into() {
+            for column_index in 0_usize..(terminal_width - 1).into() {
+                let mut true_neighbors: u8 = 0;
+                let neighbors_differences: [(isize, isize); 8] = 
+                    [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)];
+                for &(x, y) in neighbors_differences.iter() {
+                    if row_index as isize + y >= 0 || column_index as isize + x >= 0 {
+                        match cells.get((row_index as isize + y) as usize) {
+                            Some(row) => {
+                                match row.get((column_index as isize + x) as usize) {
+                                    Some(&cell) => {
+                                        if cell {true_neighbors += 1;}
+                                    },
+                                    None => {},
+                                }
+                            },
+                            None => {},
                         }
+                    }
+                }
+                match true_neighbors {
+                    0..=1 => {
+                        next_gen_cells[row_index][column_index] = false;
                     },
-                    _ => {}
+                    2 => {},
+                    3 => {
+                        next_gen_cells[row_index][column_index] = true;
+                    },
+                    _ => {
+                        next_gen_cells[row_index][column_index] = true;
+                    },
                 }
             }
-            _ => {},
         }
-        // println!("{:?}", event);
+        cells = next_gen_cells.clone();
         
+        // Print cells
+        queue!(stdout, SetBackgroundColor(CELL_COLOR));
+        for row_index in (TOP_MARGIN as usize)..(terminal_height - BOTTOM_MARGIN).into() {
+            for column_index in 0_usize..(terminal_width - 1).into() {
+                if cells[row_index][column_index] {
+                    queue!(stdout, SetBackgroundColor(CELL_COLOR));
+                } else {
+                    queue!(stdout, SetBackgroundColor(BACKGROUND_COLOR));
+                }
+                queue!(
+                    stdout,
+                    cursor::MoveTo(column_index as u16, row_index as u16),
+                    Print(' '),
+                );
+            }
+        }
     }
-
 
     // Restore terminal settings to default
     disable_raw_mode();
     queue!(stdout, DisableMouseCapture, cursor::Show, LeaveAlternateScreen);
     stdout.flush();
 }
-
